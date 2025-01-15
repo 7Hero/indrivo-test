c1490ac7edcee00e3b5116ed760ff21a3d134296
7hero <vpoopescu07@gmail.com>
2025-01-15 23:42:25 +0200

    create app layout and styles, remove unused CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import "./App.css";
-
 function App() {
-  return <div>My App</div>;
+  return (
+    <div className="page">
+      <div className="page__container">
+        {/* Mobile view */}
+        <div className="page__mobile-view"></div>
+
+        {/* Desktop view */}
+        <div className="page__desktop-view">
+          <div className="header__item">Products</div>
+          <div className="header__item">Feedback</div>
+          <div className="page__content">Products Content</div>
+          <div className="page__content"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
