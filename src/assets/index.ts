1d5d1121f07365192d700025ed7c1e0cacfbf81d
7hero <vpoopescu07@gmail.com>
2025-01-16 03:38:21 +0200

    add product assets icons

---
+++ src/assets/index.ts
@@ -0,0 +1,6 @@
+import { AirplaneIcon } from "./airplane-icon";
+import { HelicopterIcon } from "./helicopter-icon";
+import { RocketIcon } from "./rocket-icon";
+import { UFOIcon } from "./ufo-icon";
+
+export { AirplaneIcon, HelicopterIcon, RocketIcon, UFOIcon };
